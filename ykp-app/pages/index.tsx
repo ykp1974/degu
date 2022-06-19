e191f18daf56e01e17e906c6e1348b47283b5a4e
ykp1974 <yappaykp1974@gmail.com>
2022-06-19 16:49:27 +0900

    az

--- ykp-app/pages/index.tsx
+++ ykp-app/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
@@ -13,9 +14,12 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className="title">
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
+      </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
